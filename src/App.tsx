117508aae7a06b491a6ffd250d4018055935695e
pvalkoun <47795256+pvalkoun@users.noreply.github.com>
2026-04-09 21:15:08 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+     <BrowserRouter basename="/developers">  
         <ScrollToTop />
         <Routes>
           <Route element={<DocLayout />}>
